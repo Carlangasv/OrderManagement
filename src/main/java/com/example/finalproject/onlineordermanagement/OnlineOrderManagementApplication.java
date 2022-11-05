98ec78d305f7f01a662d96070b2e2204254e5b06
Carlangasv <50808838+Carlangasv@users.noreply.github.com>
2022-11-05 11:08:22 -0500

    Fresh created app

---
+++ src/main/java/com/example/finalproject/onlineordermanagement/OnlineOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalproject.onlineordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOrderManagementApplication.class, args);
+    }
+
+}
